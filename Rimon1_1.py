96728f323e814c38bc344d169974ae66d7ae92e8
Rimon Adel <35565654+RimonAdel@users.noreply.github.com>
2019-12-09 11:34:48 +0200

    Add files via upload

---
+++ Rimon1_1.py
@@ -0,0 +1,45 @@
+image_name = "images/Rimon1.1.jpg"
+scale_percent = 20
+UPPER_RATIO = 3.259259259
+LOWER_RATIO = 3.259259259
+
+right_sholder_outer_point =  (134,220)
+right_sholder_inner_point = (171, 204)
+left_sholder_inner_point = (225, 199)
+left_sholder_outer_point = (258, 222)
+
+right_elbow_outer_point = (97, 303)
+right_elbow_inner_point = (122, 320)
+left_elbow_inner_point = (270, 306)
+left_elbow_outer_point = (295,307)
+
+right_chest_arm_meeting_point = (142, 260)
+left_chest_arm_meeting_point = (253, 260)
+
+right_chest_point = (147, 338)
+left_chest_point =  (248, 338)
+
+right_waist_point = (147, 338)
+left_waist_point =  (248, 338)
+
+right_hip_point = (143, 377)
+left_hip_point = (250,377)
+
+stone_of_trousers_point = (198,434)
+
+right_Knee_outer_point = (148, 499)
+right_Knee_inner_point = (185, 499)
+
+left_Knee_inner_point = (210,500)
+left_Knee_outer_point = (248 ,500)
+
+right_ankle_outer_point = (152, 590)
+right_ankle_inner_point = (177,592)
+
+left_ankle_inner_point = (222, 592)
+left_ankle_outer_point = (245, 590)
+
+
+HIP_HIGHT = 377
+RIGHT_HIP_PIXEL = (143, 377)
+LEFT_HIP_PIXEL = (250, 377)
