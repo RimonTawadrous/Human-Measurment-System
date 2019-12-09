e91b2cb2a9e28a23d67b533900a0930b063ab4a3
Rimon Adel <35565654+RimonAdel@users.noreply.github.com>
2019-12-09 11:35:09 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 import math
 from keypoint_config import *
-
-RATIO = 3.64457831
+from Rimon2_1 import *
+    
 
 def apply_filter_return_countoures(hsv_image, hsv_color_lower, hsv_color_upper, min_contor_area = 15, img = None , contor_label=""):
     filtered_image = cv2.inRange(hsv_image, hsv_color_lower, hsv_color_upper)
@@ -13,13 +13,11 @@ def apply_filter_return_countoures(hsv_image, hsv_color_lower, hsv_color_upper, 
     res = cv2.bitwise_and(img, img, mask = filtered_image)
     if True:
         i = 0
-        print("yes")
         for pic, contour in enumerate(contors):
             area = cv2.contourArea(contour)
-            print(area)
             if(area>min_contor_area):
                 x,y,w,h = cv2.boundingRect(contour)
-                print(contor_label,x+3,y+3)
+                # print(contor_label,x+3,y+3)
                 cv2.putText(img,str(contor_label),(x,y),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255))
                 i += 1
         return (res, filtered_image, (x+3,y+3), hierarchy)
@@ -27,6 +25,40 @@ def apply_filter_return_countoures(hsv_image, hsv_color_lower, hsv_color_upper, 
     return (res, filtered_image, contors, hierarchy)
 
 def eclidian_distance(point1 , point2):
+    global HIP_PIXEL, LEFT_HIP_PIXEL, RIGHT_HIP_PIXEL
+    (x1, y1) = point1
+    (x2, y2) = point2
+    result = 0
+    mid = (LEFT_HIP_PIXEL[0]+RIGHT_HIP_PIXEL[0]) / 2
+    if y1 >= HIP_HIGHT and y2 >= HIP_HIGHT:
+        return (math.sqrt(((x1-x2)**2)+((y1-y2)**2)))/LOWER_RATIO
+    elif y1 <= HIP_HIGHT and y2 <= HIP_HIGHT:
+        return (math.sqrt(((x1-x2)**2)+((y1-y2)**2)))/UPPER_RATIO
+    else:
+        if y1 > HIP_HIGHT:
+            if x1 > mid:
+                result += ((math.sqrt(((x1-LEFT_HIP_PIXEL[0])**2)+((y1-LEFT_HIP_PIXEL[1])**2)))/LOWER_RATIO)
+            else:
+                result += ((math.sqrt(((x1-RIGHT_HIP_PIXEL[0])**2)+((y1-RIGHT_HIP_PIXEL[1])**2)))/LOWER_RATIO)
+        else:
+            if x1 > mid:
+                result += ((math.sqrt(((x1-LEFT_HIP_PIXEL[0])**2)+((y1-LEFT_HIP_PIXEL[1])**2)))/UPPER_RATIO)
+            else:
+                result += ((math.sqrt(((x1-RIGHT_HIP_PIXEL[0])**2)+((y1-RIGHT_HIP_PIXEL[1])**2)))/UPPER_RATIO)
+        if y2 > HIP_HIGHT:
+            if x2 > mid:
+                result += ((math.sqrt(((LEFT_HIP_PIXEL[0]-x2)**2)+((LEFT_HIP_PIXEL[1]-y2)**2)))/LOWER_RATIO)
+            else:
+                result += ((math.sqrt(((RIGHT_HIP_PIXEL[0]-x2)**2)+((RIGHT_HIP_PIXEL[1]-y2)**2)))/LOWER_RATIO)
+        else:
+            if x2 > mid:
+                result += ((math.sqrt(((LEFT_HIP_PIXEL[0]-x2)**2)+((LEFT_HIP_PIXEL[1]-y2)**2)))/UPPER_RATIO)
+            else:
+                result += ((math.sqrt(((RIGHT_HIP_PIXEL[0]-x2)**2)+((RIGHT_HIP_PIXEL[1]-y2)**2)))/UPPER_RATIO)
+
+    return result
+
+def eclidian_distance2(point1 , point2):
     (x1, y1) = point1
     (x2, y2) = point2
     return (math.sqrt(((x1-x2)**2)+((y1-y2)**2)))/RATIO
@@ -39,76 +71,78 @@ if __name__ == "__main__":
     "right_hip":[0,0], "stone_of_trousers":[0,0],"left_Knee_outer":[0,0], "left_Knee_inner":[0,0], "left_ankle_outer":[0,0], "left_ankle_inner":[0,0],
     "right_Knee_outer":[0,0],"right_Knee_inner":[0,0], "right_ankle_inner":[0,0], "right_ankle_outer":[0,0]}
 
-    img = cv2.imread("images/Rimon2.1.jpg", cv2.IMREAD_COLOR)
-    scale_percent = 20
-
+    img = cv2.imread(image_name, cv2.IMREAD_COLOR)
+    
+    # scaling 
     print("img.shape[1]",img.shape[1])
     print("img.shape[0]",img.shape[0])
     #calculate the 50 percent of original dimensions
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
-
-    # dsize
+    width = int(img.shape[1] * SCALE_PERCENT / 100)
+    height = int(img.shape[0] * SCALE_PERCENT / 100)
     dsize = (width, height)
-
     # resize image
     img = cv2.resize(img, dsize)
     print("img.shape[1]",img.shape[1])
     print("img.shape[0]",img.shape[0])
-    cv2.imwrite('new.jpg',img) 
+    # end of scalling
 
-    # # sholder outer
-    cv2.circle(img, (124,185), point_radius, right_sholder_outer_color, -1)
+
+    # sholder outer
+    cv2.circle(img, right_sholder_outer_point, point_radius, right_sholder_outer_color, -1)
     #right sholder inner
-    cv2.circle(img, (162,172), point_radius, right_sholder_inner_color, -1)
+    cv2.circle(img, right_sholder_inner_point, point_radius, right_sholder_inner_color, -1)
     #left sholder inner
-    cv2.circle(img, (236,172), point_radius, left_sholder_inner_color, -1)
+    cv2.circle(img, left_sholder_inner_point, point_radius, left_sholder_inner_color, -1)
     #left sholder outer
-    cv2.circle(img, (271,194), point_radius, left_sholder_outer_color, -1)
-    #right elbow outer
-    cv2.circle(img, (84,288), point_radius, right_elbow_outer_color, -1)
-    #right elbow inner
-    cv2.circle(img, (113,308), point_radius, right_elbow_inner_color, -1)
-    #left elbow inner
-    cv2.circle(img, (275,313), point_radius, left_elbow_inner_color, -1)
-    #left elbow outer
-    cv2.circle(img, (303,297), point_radius, left_elbow_outer_color, -1)
-    #right chest and arm meeting
-    cv2.circle(img, (131,233), point_radius, right_chest_arm_meeting_color, -1)
-    #left chest and arm meeting
-    cv2.circle(img, (262,239), point_radius, left_chest_arm_meeting_color, -1)
-    #right waist
-    cv2.circle(img, (135,328), point_radius, right_waist_color, -1)
-    #left waist
-    cv2.circle(img, (252,330), point_radius, left_waist_color, -1)
+    cv2.circle(img, left_sholder_outer_point, point_radius, left_sholder_outer_color, -1)
 
- 
+    #right elbow outer
+    cv2.circle(img, right_elbow_outer_point, point_radius, right_elbow_outer_color, -1)
+    #right elbow inner
+    cv2.circle(img, right_elbow_inner_point, point_radius, right_elbow_inner_color, -1)
+    #left elbow inner
+    cv2.circle(img, left_elbow_inner_point, point_radius, left_elbow_inner_color, -1)
+    #left elbow outer
+    cv2.circle(img, left_elbow_outer_point, point_radius, left_elbow_outer_color, -1)
+
+    #right chest and arm meeting
+    cv2.circle(img, right_chest_arm_meeting_point, point_radius, right_chest_arm_meeting_color, -1)
+    #left chest and arm meeting
+    cv2.circle(img, left_chest_arm_meeting_point, point_radius, left_chest_arm_meeting_color, -1)
+    
+    #right waist
+    cv2.circle(img, right_waist_point, point_radius, right_waist_color, -1)
+    #left waist
+    cv2.circle(img, left_waist_point, point_radius, left_waist_color, -1)
+
     #right hip
-    cv2.circle(img, (130,366), point_radius, right_hip_color, -1)
+    cv2.circle(img, right_hip_point, point_radius, right_hip_color, -1)
     #left hip
-    cv2.circle(img, (256,372), point_radius, left_hip_color, -1)
+    cv2.circle(img, left_hip_point, point_radius, left_hip_color, -1)
+    
     #stone of trousers
-    cv2.circle(img, (194,434), point_radius, stone_of_trousers_color, -1)
+    cv2.circle(img, stone_of_trousers_point, point_radius, stone_of_trousers_color, -1)
 
     #right Knee outer 
-    cv2.circle(img, (133,521), point_radius, right_Knee_outer_color, -1)
+    cv2.circle(img, right_Knee_outer_point, point_radius, right_Knee_outer_color, -1)
     #right Knee inner
-    cv2.circle(img, (173,522), point_radius, right_Knee_inner_color, -1)
-    #right ankle outer
-    cv2.circle(img, (132,650), point_radius, right_ankle_outer_color, -1)
-    #right ankle inner
-    cv2.circle(img, (162,650), point_radius, right_ankle_inner_color, -1)
-
-    # #left Knee inner
-    cv2.circle(img, (206,522), point_radius, left_Knee_inner_color, -1)
-    #left Knee outer
-    cv2.circle(img, (248,523), point_radius, left_Knee_outer_color, -1)
+    cv2.circle(img, right_Knee_inner_point, point_radius, right_Knee_inner_color, -1)
+    
     #left Knee inner
-    cv2.circle(img, (219,649), point_radius, left_ankle_inner_color, -1)
+    cv2.circle(img, left_Knee_inner_point, point_radius, left_Knee_inner_color, -1)
     #left Knee outer
-    cv2.circle(img, (250,649), point_radius, left_ankle_outer_color, -1)    
+    cv2.circle(img, left_Knee_outer_point, point_radius, left_Knee_outer_color, -1)
 
-
+    #right ankle outer
+    cv2.circle(img, right_ankle_outer_point, point_radius, right_ankle_outer_color, -1)
+    #right ankle inner
+    cv2.circle(img, right_ankle_inner_point, point_radius, right_ankle_inner_color, -1)
+    
+    #left Knee inner
+    cv2.circle(img, left_ankle_inner_point, point_radius, left_ankle_inner_color, -1)
+    #left Knee outer
+    cv2.circle(img, left_ankle_outer_point, point_radius, left_ankle_outer_color, -1)    
+    
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     #hsv hue set value  (color)
 
@@ -151,28 +185,22 @@ if __name__ == "__main__":
     (res,yellow,body_keypoints["right_ankle_outer"],_) = apply_filter_return_countoures(hsv,right_ankle_outer_lower,right_ankle_outer_upper,min_contor_area= 30, img=img,contor_label="RAO")
 
     # print(body_keypoints("right_sholder_outer"))
-    print(eclidian_distance((190,608),(190,113)))
-    print("sholder = ",eclidian_distance(body_keypoints["right_sholder_outer"],body_keypoints["left_sholder_outer"]))
-    print("waist = ",eclidian_distance(body_keypoints["right_waist"],body_keypoints["left_waist"]))
-    print("hip = ",eclidian_distance(body_keypoints["left_hip"],body_keypoints["right_hip"]))
-    print("under arm  = ",eclidian_distance(body_keypoints["left_chest_arm_meeting"],body_keypoints["right_chest_arm_meeting"]))
-    print("hip knee left  = ",eclidian_distance(body_keypoints["left_hip"],body_keypoints["left_Knee_outer"]))
-    print("hip knee right  = ",eclidian_distance(body_keypoints["right_hip"],body_keypoints["right_Knee_outer"]))
-    print("waist knee left  = ",eclidian_distance(body_keypoints["left_waist"],body_keypoints["left_Knee_outer"]))
-    print("waist knee right  = ",eclidian_distance(body_keypoints["right_waist"],body_keypoints["right_Knee_outer"]))
-    print("waist hip left  = ",eclidian_distance(body_keypoints["left_waist"],body_keypoints["left_hip"]))
-    print("waist hip right  = ",eclidian_distance(body_keypoints["right_waist"],body_keypoints["right_hip"]))
-    print("knee left  = ",eclidian_distance(body_keypoints["left_Knee_outer"],body_keypoints["left_Knee_inner"]))
-    print("knee right  = ",eclidian_distance(body_keypoints["right_Knee_inner"],body_keypoints["right_Knee_outer"]))
-    print("knee to ankle right = ",eclidian_distance(body_keypoints["right_Knee_outer"],body_keypoints["right_ankle_outer"]))
-    print("knee to ankle left  = ",eclidian_distance(body_keypoints["left_Knee_outer"],body_keypoints["left_ankle_outer"]))
-    print("T stone to left knee  = ",eclidian_distance(body_keypoints["stone_of_trousers"],body_keypoints["left_Knee_inner"]))
-    print("T stone to right knee  = ",eclidian_distance(body_keypoints["stone_of_trousers"],body_keypoints["right_Knee_inner"]))
-    
-    print("knee to ankle right = ",eclidian_distance((133,521),(132,650)))
-    # print("knee to ankle left  = ",eclidian_distance(body_keypoints["left_Knee_outer"],body_keypoints["left_ankle_outer"]))
-    # print("T stone to left knee  = ",eclidian_distance(body_keypoints["stone_of_trousers"],body_keypoints["left_Knee_inner"]))
-    # print("T stone to right knee  = ",eclidian_distance(body_keypoints["stone_of_trousers"],body_keypoints["right_Knee_inner"]))
+    print("sholder = ",eclidian_distance(body_keypoints["right_sholder_outer"],body_keypoints["left_sholder_outer"]),"-->",eclidian_distance2(body_keypoints["right_sholder_outer"],body_keypoints["left_sholder_outer"]))
+    print("waist = ",eclidian_distance(body_keypoints["right_waist"],body_keypoints["left_waist"]),"-->",eclidian_distance2(body_keypoints["right_waist"],body_keypoints["left_waist"]))
+    print("hip = ",eclidian_distance(body_keypoints["left_hip"],body_keypoints["right_hip"]),"-->",eclidian_distance2(body_keypoints["left_hip"],body_keypoints["right_hip"]))
+    print("under arm  = ",eclidian_distance(body_keypoints["left_chest_arm_meeting"],body_keypoints["right_chest_arm_meeting"]),"-->",eclidian_distance2(body_keypoints["left_chest_arm_meeting"],body_keypoints["right_chest_arm_meeting"]))
+    print("hip knee left  = ",eclidian_distance(body_keypoints["left_hip"],body_keypoints["left_Knee_outer"]),"-->",eclidian_distance2(body_keypoints["left_hip"],body_keypoints["left_Knee_outer"]))
+    print("hip knee right  = ",eclidian_distance(body_keypoints["right_hip"],body_keypoints["right_Knee_outer"]),"-->",eclidian_distance2(body_keypoints["right_hip"],body_keypoints["right_Knee_outer"]))
+    print("waist knee left  = ",eclidian_distance(body_keypoints["left_waist"],body_keypoints["left_Knee_outer"]),"-->",eclidian_distance2(body_keypoints["left_waist"],body_keypoints["left_Knee_outer"]))
+    print("waist knee right  = ",eclidian_distance(body_keypoints["right_waist"],body_keypoints["right_Knee_outer"]),"-->",eclidian_distance2(body_keypoints["right_waist"],body_keypoints["right_Knee_outer"]))
+    print("waist hip left  = ",eclidian_distance(body_keypoints["left_waist"],body_keypoints["left_hip"]),"-->",eclidian_distance2(body_keypoints["left_waist"],body_keypoints["left_hip"]))
+    print("waist hip right  = ",eclidian_distance(body_keypoints["right_waist"],body_keypoints["right_hip"]),"-->",eclidian_distance2(body_keypoints["right_waist"],body_keypoints["right_hip"]))
+    print("knee left  = ",eclidian_distance(body_keypoints["left_Knee_outer"],body_keypoints["left_Knee_inner"]),"-->",eclidian_distance2(body_keypoints["left_Knee_outer"],body_keypoints["left_Knee_inner"]))
+    print("knee right  = ",eclidian_distance(body_keypoints["right_Knee_inner"],body_keypoints["right_Knee_outer"]),"-->",eclidian_distance2(body_keypoints["right_Knee_inner"],body_keypoints["right_Knee_outer"]))
+    print("knee to ankle right = ",eclidian_distance(body_keypoints["right_Knee_outer"],body_keypoints["right_ankle_outer"]),"-->",eclidian_distance2(body_keypoints["right_Knee_outer"],body_keypoints["right_ankle_outer"]))
+    print("knee to ankle left  = ",eclidian_distance(body_keypoints["left_Knee_outer"],body_keypoints["left_ankle_outer"]),"-->",eclidian_distance2(body_keypoints["left_Knee_outer"],body_keypoints["left_ankle_outer"]))
+    print("T stone to left knee  = ",eclidian_distance(body_keypoints["stone_of_trousers"],body_keypoints["left_Knee_inner"]),"-->",eclidian_distance2(body_keypoints["stone_of_trousers"],body_keypoints["left_Knee_inner"]))
+    print("T stone to right knee  = ",eclidian_distance(body_keypoints["stone_of_trousers"],body_keypoints["right_Knee_inner"]),"-->",eclidian_distance2(body_keypoints["stone_of_trousers"],body_keypoints["right_Knee_inner"]))
     
     cv2.imshow("hsv", hsv)
     cv2.imshow("img", img)
